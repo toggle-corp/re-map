4d913bea13c9bf7ac241566b890ec8892808edb5
tnagorra <weathermist@gmail.com>
2021-07-13 12:33:44 +0545

    Change build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,16 @@
+const pkg = require('./package.json');
+
 module.exports = {
     extends: [
         'airbnb',
-        'plugin:css-modules/recommended',
+        'airbnb/hooks',
         'plugin:@typescript-eslint/recommended',
     ],
     env: {
         browser: true,
         jest: true,
     },
-    globals: {
-        chrome: true,
-    },
     plugins: [
-        'react',
-        'react-hooks',
-        'import',
-        'css-modules',
         '@typescript-eslint',
     ],
     settings: {
@@ -23,37 +18,7 @@ module.exports = {
             'babel-module': {
                 root: ['.'],
                 extensions: ['.js', '.jsx', '.ts', '.tsx'],
-                alias: {
-                    '#components': './src/components',
-                    '#dui': './src/components/ui',
-                    '#config': './src/config',
-                    '#constants': './src/constants',
-                    '#cs': './src/cs',
-                    '#entities': './src/entities',
-                    '#notify': './src/notify',
-                    '#redux': './src/redux',
-                    '#request': './src/request',
-                    '#resources': './src/resources',
-                    '#rest': './src/rest',
-                    '#hooks': './src/hooks',
-                    '#rsca': './src/vendor/react-store/components/Action',
-                    '#rscg': './src/vendor/react-store/components/General',
-                    '#rsci': './src/vendor/react-store/components/Input',
-                    '#rscv': './src/vendor/react-store/components/View',
-                    '#rscz': './src/vendor/react-store/components/Visualization',
-                    '#rsk': './src/vendor/react-store/constants',
-                    '#rsu': './src/vendor/react-store/utils',
-                    '#schema': './src/schema',
-                    '#store': './src/store',
-                    '#theme': './src/theme',
-                    '#ts': './src/ts',
-                    '#utils': './src/utils',
-                    '#views': './src/views',
-                    '#widgetComponents': './src/widgetComponents',
-                    '#widgets': './src/widgets',
-                    '#typings': './src/typings',
-                    '#qbc': './src/components/questionnaire-builder',
-                },
+                alias: {},
             },
         },
         react: {
@@ -70,28 +35,20 @@ module.exports = {
         allowImportExportEverywhere: true,
     },
     rules: {
-        strict: 0,
+        strict: 1,
         indent: ['error', 4, { SwitchCase: 1 }],
+
         'no-console': 0,
 
-        camelcase: 'warn',
+        'no-unused-vars': 0,
+        '@typescript-eslint/no-unused-vars': 1,
 
-        'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4],
-        'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-        'react/prop-types': [1, { ignore: [], customValidators: [], skipUndeclared: false }],
-        'react/forbid-prop-types': [1],
+        'no-use-before-define': 0,
+        '@typescript-eslint/no-use-before-define': 1,
 
-        'jsx-a11y/anchor-is-valid': ['error', {
-            components: ['Link'],
-            specialLink: ['to'],
-        }],
-
-        'import/extensions': ['off', 'never'],
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-
-        'css-modules/no-unused-class': [1, { camelCase: true }],
-        'css-modules/no-undef-class': [1, { camelCase: true }],
+        // note you must disable the base rule as it can report incorrect errors
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
 
         'prefer-destructuring': 'warn',
         'function-paren-newline': ['warn', 'consistent'],
@@ -102,35 +59,49 @@ module.exports = {
             ExportDeclaration: { consistent: true },
         }],
 
+        'import/no-unresolved': ['error', { ignore: Object.keys(pkg.peerDependencies) }],
+        'import/extensions': ['off', 'never'],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+
+        '@typescript-eslint/no-empty-interface': 0,
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+
+        'jsx-a11y/anchor-is-valid': ['error', {
+            components: ['Link'],
+            specialLink: ['to'],
+        }],
         'jsx-a11y/label-has-for': 'warn',
 
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error'],
-
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-
-        'no-shadow': 'off',
-        '@typescript-eslint/no-shadow': ['error'],
-
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-        'react/sort-comp': 'off',
-
-        '@typescript-eslint/no-empty-interface': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/camelcase': 'off',
-
-        'react/prefer-stateless-function': 'warn',
-        'import/no-named-as-default': 'warn',
-
+        'react/prop-types': 0,
+        'react/jsx-props-no-spreading': 0,
+        // 'react/prop-types': [1, { ignore: [], customValidators: [], skipUndeclared: false }],
         'react/no-unused-state': 'warn',
+        'react/require-default-props': ['warn', { ignoreFunctionalComponents: true }],
         'react/default-props-match-prop-types': ['warn', {
             allowRequiredDefaults: true,
         }],
-        'react/require-default-props': 'warn',
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'react/forbid-prop-types': [1],
+        'react/destructuring-assignment': [1, 'always', { ignoreClassFields: true }],
+        'react/sort-comp': [1, {
+            order: [
+                'static-methods',
+                'constructor',
+                'lifecycle',
+                'everything-else',
+                'render',
+            ],
+        }],
 
         'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/exhaustive-deps': [
+            'warn',
+            {
+                additionalHooks: 'useDidUpdateEffect',
+            },
+        ],
     },
 };
